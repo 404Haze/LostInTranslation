99b0c49cc1dfde4d43ad16251a7cdf4d0f6b4bcb
albertkang1004 <albertkang1004@gmail.com>
2025-09-22 16:24:10 -0400

    test

--- src/main/java/translation/CountryCodeConverter.java
+++ src/main/java/translation/CountryCodeConverter.java
@@ -57,7 +57,8 @@ public class CountryCodeConverter {
      * @return the name of the country corresponding to the code
      */
     public String fromCountryCode(String code) {
-        return countryCodeToCountry.get(code);
+        return "sample";
+        //return countryCodeToCountry.get(code);
     }
 
     /**
